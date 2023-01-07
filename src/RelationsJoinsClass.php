48431bad8847aedbff2ff94c67abc32c96ed220f
SalvatoreCervone <40053308+SalvatoreCervone@users.noreply.github.com>
2023-01-07 19:06:52 +0100

    Class update

--- src/RelationsJoinsClass.php
+++ src/RelationsJoinsClass.php
@@ -4,4 +4,51 @@ namespace SalvatoreCervone\RelationsJoins;
 
 class RelationsJoinsClass
 {
+      static function init(Model $model, array $relation)
+    {
+
+        $model->load($relation);
+        $mapModelType = self::getDataRelations($model);
+        return $mapModelType;
+    }
+
+    static function getDataRelations($model)
+    {
+        $modelRelation = null;
+        $relations = $model->getRelations();
+        foreach ($relations as $key => $value) {
+            $type = get_class($model->{$key}());
+            $data = [];
+            if ($type == HasMany::class) {
+
+                $data = [
+                    'nome' => $key,
+                    'classe' => get_class($model->{$key}()->getRelated()),
+                    'tipo' => $type,
+                    'tablerelation' => $model->{$key}()->getRelated()->getTable(),
+                    'foreignkey' => $model->{$key}()->getQualifiedForeignKeyName(),
+                    'parentkey' => $model->{$key}()->getQualifiedParentKeyName()
+                ];
+                $data['join'] = 'inner join ' . $data['tablerelation'] . ' on ' . $data['foreignkey'] . '=' . $data['parentkey'];
+                $modelRelation[] = $data;
+            } elseif ($type == BelongsToMany::class) {
+                $data = [
+                    'nome' => $key,
+                    'classe' => get_class($model->{$key}()->getRelated()),
+                    'tipo' => $type,
+                    'tablepivot' => $model->{$key}()->getTable(),
+                    'tablerelation' => $model->{$key}()->getRelated()->getTable(),
+                    'parentkey' => $model->{$key}()->getQualifiedParentKeyName(),
+                    'parentforeignkey' => $model->{$key}()->getQualifiedRelatedPivotKeyName(),
+                    'relationkey' => $model->{$key}()->getQualifiedRelatedKeyName(),
+                    'relationforeignkey' => $model->{$key}()->getQualifiedForeignPivotKeyName(),
+                ];
+                $data['join'] = 'inner join ' . $data['tablepivot'] . ' on ' . $data['parentforeignkey'] . '=' . $data['parentkey']  .
+                    ' inner join ' . $data['tablerelation'] . ' on ' . $data['relationkey'] . '=' . $data['relationforeignkey'];
+
+                $modelRelation[] = $data;
+            }
+        }
+        return $modelRelation;
+    }
 }
